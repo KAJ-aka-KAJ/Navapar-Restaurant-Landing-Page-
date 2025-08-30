d05266928f428bc12da698e2d885e04f41d4e58d
KAJ-aka-KAJ <Kajofficialg@gmail.com>
2025-08-30 08:20:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// JavaScript functionality will be added section by section
 console.log("Navapar Restaurant site loaded.");
+
 // Mobile navbar toggle
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
@@ -8,24 +8,20 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   hamburger.classList.toggle("open");
 });
+
 // Hero Background Slider
 const hero = document.querySelector(".hero");
 
-// Replace with your food-related images
 const heroImages = [
   "https://www.santaanasweets.com/wp-content/uploads/scallops-7404270_1280-768x512.jpg.webp",
   "https://www.allrecipes.com/thmb/wrNZyC416h41hqFoVno3xdR-zCI=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/49404-Juiciest-Hamburgers-Ever-mfs-050-0ac3bef367714d89a1c219d9cfdb9186.jpg",
-  "background-image: url('https://www.allrecipes.com/thmb/ZkkFR4ljzYNYj7UulkhFVUe0wE4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/LorrainesClubSandwich_AR30Min_038_Photo-by-Allrecipes-1-df96417adce0448c80ba3a7b7631cac7.jpg"
+  "https://www.allrecipes.com/thmb/ZkkFR4ljzYNYj7UulkhFVUe0wE4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/LorrainesClubSandwich_AR30Min_038_Photo-by-Allrecipes-1-df96417adce0448c80ba3a7b7631cac7.jpg"
 ];
 
 let currentImageIndex = 0;
 
 function changeHeroBackground() {
-  hero.style.setProperty(
-    "--hero-bg",
-    `url(${heroImages[currentImageIndex]})`
-  );
-  hero.style.setProperty("background-image", `url(${heroImages[currentImageIndex]})`);
+  hero.style.backgroundImage = `url(${heroImages[currentImageIndex]})`;
   currentImageIndex = (currentImageIndex + 1) % heroImages.length;
 }
 
