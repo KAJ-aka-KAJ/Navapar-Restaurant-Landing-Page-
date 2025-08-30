3856578d86d0809413787981ce9f1510e83e3979
KAJ-aka-KAJ <Kajofficialg@gmail.com>
2025-08-30 08:26:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,22 +9,18 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
 });
 
-// Hero Background Slider
 const hero = document.querySelector(".hero");
-
 const heroImages = [
   "https://www.santaanasweets.com/wp-content/uploads/scallops-7404270_1280-768x512.jpg.webp",
   "https://www.allrecipes.com/thmb/wrNZyC416h41hqFoVno3xdR-zCI=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/49404-Juiciest-Hamburgers-Ever-mfs-050-0ac3bef367714d89a1c219d9cfdb9186.jpg",
   "https://www.allrecipes.com/thmb/ZkkFR4ljzYNYj7UulkhFVUe0wE4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/LorrainesClubSandwich_AR30Min_038_Photo-by-Allrecipes-1-df96417adce0448c80ba3a7b7631cac7.jpg"
 ];
-
-let currentImageIndex = 0;
+let current = 0;
 
 function changeHeroBackground() {
-  hero.style.backgroundImage = `url(${heroImages[currentImageIndex]})`;
-  currentImageIndex = (currentImageIndex + 1) % heroImages.length;
+  hero.style.backgroundImage = `url(${heroImages[current]})`;
+  current = (current + 1) % heroImages.length;
 }
 
-// Initialize
 changeHeroBackground();
-setInterval(changeHeroBackground, 5000); // Change every 5s
+setInterval(changeHeroBackground, 5000);
